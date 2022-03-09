f99b7f514074b499b6f833ca0b558ef822839991
Senninseyi <seyi.oyebamiji@gmail.com>
2022-03-09 01:27:02 +0100

    Create works.js

---
+++ src/pages/works.js
@@ -0,0 +1,15 @@
+import React from "react";
+import Header from "../components/navigation";
+
+function Works() {
+    return(
+        <div>
+            <Header/>
+            <main className="mt-24">
+                hello
+            </main>
+        </div>
+    );
+};
+
+export default Works;
