02e02462244b2b680d6d53d7da96042c58ccd2f5
Senninseyi <seyi.oyebamiji@gmail.com>
2022-03-09 01:26:59 +0100

    Create main.js

---
+++ src/pages/main.js
@@ -0,0 +1,12 @@
+import React from "react";
+import Header from "../components/navigation";
+
+function Portfolio() {
+    return(
+        <div>
+            <Header/>
+        </div>
+    );
+};
+
+export default Portfolio;
